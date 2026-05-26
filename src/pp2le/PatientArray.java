c20f11661e79922239207d1c4aaf2e1562a0dfc3
Gelly <angellugpatan@gmail.com>
2026-05-27 00:28:40 +0800

    update

--- src/pp2le/PatientArray.java
+++ src/pp2le/PatientArray.java
@@ -4,10 +4,57 @@
  */
 package pp2le;
 
+import java.util.ArrayList;
 /**
  *
  * @author Allysa Maravilla
  */
 public class PatientArray {
-    
+
+    private ArrayList<StudentPatient> patientList;
+
+    public PatientArray() {
+
+        patientList = new ArrayList<>();
+
+    }
+
+    // ADD PATIENT
+    public void addPatient(StudentPatient patient) {
+
+        patientList.add(patient);
+
+    }
+
+    // DELETE PATIENT
+    public void deletePatient(StudentPatient patient) {
+
+        patientList.remove(patient);
+
+    }
+
+    // RETURN ALL PATIENTS
+    public ArrayList<StudentPatient> getPatientList() {
+
+        return patientList;
+
+    }
+
+    // SEARCH BY STUDENT ID
+    public StudentPatient searchPatient(String studentID) {
+
+        for (StudentPatient patient : patientList) {
+
+            if (patient.getStudentID().equals(studentID)) {
+
+                return patient;
+
+            }
+
+        }
+
+        return null;
+
+    }
+
 }
